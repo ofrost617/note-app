be57a98c8af01d514773ac6efef91c119a4cf37c
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-09-12 13:24:37 +0100

    First note is working

--- src/note.js
+++ src/note.js
@@ -1,7 +1,16 @@
-(function(exports) {
-  function Note() {
-    this.posts = 5;
-  };
+// (function(exports) {
+//   function Note() {
+//     this.posts = 5;
+//   };
 
-  exports.Note = Note;
-})(this);
+//   exports.Note = Note;
+// })(this);
+
+var Note = function(string){
+	this.note = string;
+};
+
+function makeNote(string) {
+	var note = new Note(string)
+	return note;
+};
