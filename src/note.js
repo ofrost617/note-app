c8c803b4931a8f6e8f3a4f0a3fa53d90ecbd4102
Olivia Frost <olivia.frost617@googlemail.com>
2017-09-13 11:47:16 +0100

    Added Denis code and updated src route in index.html for note.js

--- src/note.js
+++ src/note.js
@@ -2,15 +2,24 @@
 //   function Note() {
 //     this.posts = 5;
 //   };
-
 //   exports.Note = Note;
 // })(this);
-
-var Note = function(string){
-	this.note = string;
-};
-
-function makeNote(string) {
-	var note = new Note(string)
-	return note;
-};
+// var Note = function(string){
+//  this.note = string;
+// };
+// function makeNote(string) {
+//  var note = new Note(string)
+//  return note;
+// };
+var btn = document.getElementById("btn");
+var saved = []
+btn.addEventListener("click", function() {
+    var newNote = document.getElementById("noteForm").value;
+    saved.push(newNote)
+    // renderHTML
+    console.log(saved);
+});
+var listNote = document.getElementById("listofNotes")
+function renderHTML() {
+    listNote.insertAdjacentHTML("afterend", newNote);
+}
