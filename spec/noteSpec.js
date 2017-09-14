186974a361ce092eae193c480d63b45350b3b562
alessiobortone2 <alessiobortone2@gmail.com>
2017-09-14 12:37:32 +0100

    Test

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,6 +1,7 @@
-function testNotePostsToBe5() {
-  var note = new Note();
-  assert.isTrue(note.posts === 5);
-};
-
-testNotePostsToBe5();
+//
+// function testNotePostsToBe5() {
+//   var note = new Note();
+//   assert.isTrue(note.posts === 5);
+// };
+//
+// testNotePostsToBe5();
--- src/note.js
+++ src/note.js
@@ -1,7 +1,35 @@
-(function(exports) {
-  function Note() {
-    this.posts = 5;
-  };
+// (function(exports) {
+//   function Note() {
+//     this.posts = 5;
+//   };
+//
+//   exports.Note = Note;
+// })(this);
+//
 
-  exports.Note = Note;
-})(this);
+
+//
+// var listNote = document.getElementById("listofNotes")
+//
+// function renderHTML() {
+//     listNote.insertAdjacentHTML("afterend", newNote);
+// }
+//
+// function Note(content) {
+//     this.content = content;
+// };
+// var note = new Note(string);
+//
+// function createNote() {
+//   var btn = document.getElementById("btn");
+//   var list = [];
+//   btn.addEventListener("click", function() {
+//     var string = document.getElementById("noteForm").value;
+//     list.push(note);
+//     console.log(list);
+//   });
+// };
+//
+// function displayNote(note) {
+//   document.getElementById("listOfNotes").innerHTML = note.content;
+// };
